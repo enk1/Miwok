a834383f732513f98c8d86a40829a3bf7570fb0f
enk1 <grzywaczewski.jakub@gmail.com>
2017-06-20 08:05:22 +0200

    ok

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,17 +8,28 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId;
 
     public Word(String defaultTranslation, String miwokTranslation){
         this.mDefaultTranslation = defaultTranslation;
         this.mMiwokTranslation = miwokTranslation;
     }
 
+    public Word(String mDefaultTranslation, String mMiwokTranslation, int mImageResourceId) {
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mMiwokTranslation = mMiwokTranslation;
+        this.mImageResourceId = mImageResourceId;
+    }
+
     public String getMiwokTranslation(){
         return mMiwokTranslation;
     }
+
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
 
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -21,7 +22,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
      * to populate into the lists.
      *
      * @param context        The current context. Used to inflate the layout file.
-     * @param androidFlavors A List of AndroidFlavor objects to display in a list
+     * @param words A List of AndroidFlavor objects to display in a list
      */
 
     public WordAdapter(Activity context, ArrayList<Word> words) {
@@ -54,12 +55,13 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         // Find the TextView in the list_item.xml layout with the ID version_name
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
-        // Get the version name from the current AndroidFlavor object and
-        // set this text on the name TextView
         miwokTextView.setText(currentWordFlavor.getMiwokTranslation());
 
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(currentWordFlavor.getDefaultTranslation());
+
+        ImageView iconView = (ImageView) listItemView.findViewById(R.id.image_text_view);
+        iconView.setImageResource(currentWordFlavor.getImageResourceId());
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
